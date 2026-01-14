2d4858bc5f26f01871f64447756f501775f09cb0
Bogdan <bogdan.iluschenko@gmail.com>
2026-01-14 20:37:56 +0700

    Common-Footer

--- frontend/MusicHostingWebApp/src/components/Footer/Footer.tsx
+++ frontend/MusicHostingWebApp/src/components/Footer/Footer.tsx
@@ -1,3 +1,12 @@
-function Footer() {}
+function Footer() {
+    return (
+        <div className='Footer'>
+            <button className='Footer__navigation'>Лента</button>
+            <button className='Footer__navigation'>Моя музыка</button>
+            <button className='Footer__navigation'>Топы</button>
+            <button className='Footer__navigation'>Профиль</button>
+        </div>
+    );
+}
 
 export default Footer;
