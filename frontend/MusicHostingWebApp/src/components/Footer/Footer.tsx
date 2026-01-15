e46e90ea4f2ae8ba23303e655a963694db3ecccf
Bogdan <bogdan.iluschenko@gmail.com>
2026-01-15 22:24:32 +0700

    Footer-active-options

--- frontend/MusicHostingWebApp/src/components/Footer/Footer.tsx
+++ frontend/MusicHostingWebApp/src/components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {FiHome, FiUser} from 'react-icons/fi';
 import {LuLibrary} from 'react-icons/lu';
 import {IoMdTrendingUp} from 'react-icons/io';
@@ -5,32 +6,63 @@ import {IoMdTrendingUp} from 'react-icons/io';
 import './Footer.scss';
 
 function Footer() {
+    const ref = React.useRef(null);
+
+    const handleClick = (event: React.MouseEvent<HTMLLabelElement>) => {
+        const activeFooterAction = event.currentTarget;
+        if (ref.current) {
+            const footer:HTMLElement = ref.current;
+            const lebels = footer.querySelectorAll('*');
+            lebels.forEach( lebel => {
+                if (lebel instanceof HTMLElement) {
+                    lebel.style.color = '';
+                }
+            });
+        }
+        activeFooterAction.style.color = '#ad6de4';
+    };
+
     return (
-        <div className='Footer'>
-            <button className='Footer__navigation'>
-                <label className='Footer__text'>
-                    <FiHome  size={20} />
+        <div 
+            className='Footer'
+            ref={ref}
+        >
+            <div className='Footer__navigation'>
+                <label
+                    className='Footer__text'
+                    onClick={handleClick}
+                >
+                    <FiHome size={20} />
                     Лента
                 </label>
-            </button>
-            <button className='Footer__navigation'>
-                <label className='Footer__text'>
+            </div>
+            <div className='Footer__navigation'>
+                <label
+                    className='Footer__text'
+                    onClick={handleClick}
+                >
                     <LuLibrary size={20} />
                     Моя музыка
                 </label>
-            </button>
-            <button className='Footer__navigation'>
-                <label className='Footer__text'>
+            </div>
+            <div className='Footer__navigation'>
+                <label 
+                    className='Footer__text'
+                    onClick={handleClick}
+                >
                     <IoMdTrendingUp size={20} />
                     Топы
                 </label>
-            </button>
-            <button className='Footer__navigation'>
-                <label className='Footer__text'>
+            </div>
+            <div className='Footer__navigation'>
+                <label
+                    className='Footer__text'
+                    onClick={handleClick}
+                >
                     <FiUser size={20} />
                     Профиль
                 </label>
-            </button>
+            </div>
         </div>
     );
 }
